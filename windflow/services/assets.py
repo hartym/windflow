970e5634b65604c30413909576490ac7495dd90a
Romain Dorgueil <romain@dorgueil.net>
2016-10-22 12:09:16 +0200

    new assets class allows not to return 500 when assets.json is not yet available

--- windflow/services/assets.py
+++ windflow/services/assets.py
@@ -2,6 +2,30 @@ import simplejson
 from windflow.services import Service
 
 
+class Assets(dict):
+    def get_style(self, name):
+        bundle = self[name]
+        if 'css' in bundle:
+            return '<link href="' + bundle['css'] + '" rel="stylesheet">'
+        return ''
+
+    def get_script(self, name):
+        bundle = self[name]
+        if 'js' in bundle:
+            return '<script src="' + bundle['js'] + '" type="text/javascript"></script>'
+        return ''
+
+
+class UnavailableAssets(Assets):
+    def get_style(self, name):
+        return ''
+
+    def get_script(self, name):
+        return ('<script type="text/javascript">document.write("Assets are not available. It may means that the '
+                'bundling process is still running, or that the webpack AssetsPlugin did not run. Look for the '
+                'assets.json file in your static directory (and look at webpack output).")</script>')
+
+
 class WebpackAssets(Service):
     """
     Webpack assets service.
@@ -15,8 +39,8 @@ class WebpackAssets(Service):
     def get(self):
         try:
             with open(self.filename) as f:
-                return simplejson.load(f)
+                return Assets(simplejson.load(f))
         except:
             # todo this is evil, but we'd need to find out errors e do want to "ignore" or how to tell user about it.
             # at the very minimum: simplejson.scanner.JSONDecodeError and probably missing file
-            return {}
+            return UnavailableAssets()
